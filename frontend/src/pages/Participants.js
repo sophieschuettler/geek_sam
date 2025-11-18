3910270b22f76794745a63b054a411a1fc1e7576
Sophie Schüttler <62881145+sophieschuettler@users.noreply.github.com>
2025-11-18 10:37:55 +0100

    social media

--- frontend/src/pages/Participants.js
+++ frontend/src/pages/Participants.js
@@ -126,7 +126,7 @@ const role = user?.role;
 
               {selectedParticipant.link && (
                 <p className="mb-3">
-                  <span className="font-semibold">Instagram: </span>
+                 
                   <a
                     href={
                       selectedParticipant.link.startsWith("http")
@@ -137,7 +137,7 @@ const role = user?.role;
                     rel="noopener noreferrer"
                     className="text-blue-600 hover:text-blue-800 underline"
                   >
-                    {selectedParticipant.link.replace(/^https?:\/\//, "")}
+                    Social Media 
                   </a>
                 </p>
               )}
