96bbddda8857138bb420b45512f9d224ad0cda25
Sophie Schüttler <62881145+sophieschuettler@users.noreply.github.com>
2026-06-02 14:23:53 +0200

    vercel

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const BASE_URL = process.env.BACKEND_URL || `http://localhost:${PORT}`;
 const allowedOrigins = [
   "http://localhost:3000",              // lokal (zum Testen)
   "http://192.168.1.131:3000",          // Handy im WLAN
-     // <– HIER deine echte Vercel-URL eintragen!
+   "https://geek-sam.vercel.app/"  // <– HIER deine echte Vercel-URL eintragen!
 ];
 app.use(cors({
   origin: allowedOrigins,
