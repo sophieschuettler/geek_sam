fbb160df1efe762afdc9d4456791c293fcd323fe
Sophie Schüttler <62881145+sophieschuettler@users.noreply.github.com>
2026-06-02 15:58:58 +0200

    logout und switch getauscht

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -113,14 +113,7 @@ export default function Navbar() {
               </NavLink>
             ))}
 
-            {user && (
-              <button
-                onClick={handleLogout}
-                className="ml-3 px-4 py-2 rounded bg-red-600 hover:bg-red-700"
-              >
-                Logout
-              </button>
-            )}
+            
 
             <FormGroup className="ml-4">
               <FormControlLabel
@@ -130,6 +123,15 @@ export default function Navbar() {
                 label=""
               />
             </FormGroup>
+            {user && (
+              <button
+                onClick={handleLogout}
+                className="ml-3 px-4 py-2 rounded bg-red-600 hover:bg-red-700"
+              >
+                Logout
+              </button>
+            )}
+
           </div>
 
           {/* Mobile */}
