8199ee16eb2395fcb4f8d35b81b69f5fa4e62af6
Sophie Schüttler <62881145+sophieschuettler@users.noreply.github.com>
2026-06-04 22:44:04 +0200

    waedeqe

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -74,6 +74,7 @@ const logout = () => {
 
 
   const links = [
+
     { to: "/teilnehmer", label: "Teilnehmer" },
     { to: "/costume", label: "Kostüm" },
     { to: "/performance", label: "Performance" },
@@ -86,8 +87,13 @@ const logout = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16 items-center">
 
-          <img src={Logo} alt="Logo" className="w-[150px]" />
 
+          <img
+            src={Logo}
+            alt="Logo"
+            className="w-[150px] cursor-pointer"
+            onClick={() => navigate("/dashboard")}
+          />
 
           {/* Desktop */}
           <div className="hidden md:flex space-x-4 items-center">
